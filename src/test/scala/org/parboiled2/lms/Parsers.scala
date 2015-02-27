9485c365fecac2a210a1592a48f1b983bb3558c1
Alexander Myltsev <alexander.myltsev@phystech.edu>
2015-02-27 23:33:45 +0300

    Add first bits of parboiled2

---
+++ src/test/scala/org/parboiled2/lms/Parsers.scala
@@ -0,0 +1,96 @@
+package org.parboiled2.lms
+
+import scala.lms.tutorial.{DslDriver, DslGen, TutorialFunSuite}
+import scala.virtualization.lms.common.{Base, BaseExp, ScalaGenBase}
+
+trait ParboiledOps extends Base {
+  implicit def string2rep(s: String): Rep[Rule] = str(unit(s))
+
+  trait Rule
+
+  def str(s: Rep[String]): Rep[Rule]
+  def infix_~(lhs: Rep[Rule], rhs: Rep[Rule]): Rep[Rule]
+  def infix_|(lhs: Rep[Rule], rhs: Rep[Rule]): Rep[Rule]
+  def zeroOrMore(r: Rep[Rule]): Rep[Rule]
+  def oneOrMore(r: Rep[Rule]): Rep[Rule]
+  def optional(r: Rep[Rule]): Rep[Rule]
+  def &(r: Rep[Rule]): Rep[Rule]
+  def not(r: Rep[Rule]): Rep[Rule]
+  def rec(r: => Rep[Rule]): Rep[Rule]
+
+  def infix_parse(r: Rep[Rule], input: Rep[String]): Rep[Boolean]
+}
+
+trait ParboiledOpsExp extends ParboiledOps with BaseExp {
+  case class StringLiteral(str: Exp[String]) extends Def[Rule]
+  case class Sequence(lhs: Exp[Rule], rhs: Exp[Rule]) extends Def[Rule]
+  case class FirstOf(lhs: Exp[Rule], rhs: Exp[Rule]) extends Def[Rule]
+  case class ZeroOrMore(r: Exp[Rule]) extends Def[Rule]
+  case class OneOrMore(r: Exp[Rule]) extends Def[Rule]
+  case class Optional(r: Exp[Rule]) extends Def[Rule]
+  case class AndPredicate(r: Exp[Rule]) extends Def[Rule]
+  case class NotPredicate(r: Exp[Rule]) extends Def[Rule]
+  case class Recursive(r: () => Exp[Rule]) extends Def[Rule]
+  case class Parse(r: Exp[Rule], str: Exp[String]) extends Def[Boolean]
+
+  def str(s: Exp[String]): Exp[Rule] = StringLiteral(s)
+  def infix_~(lhs: Exp[Rule], rhs: Exp[Rule]): Exp[Rule] = Sequence(lhs, rhs)
+  def infix_|(lhs: Exp[Rule], rhs: Exp[Rule]): Exp[Rule] = FirstOf(lhs, rhs)
+  def zeroOrMore(r: Exp[Rule]): Exp[Rule] = ZeroOrMore(r)
+  def oneOrMore(r: Exp[Rule]): Exp[Rule] = OneOrMore(r)
+  def optional(r: Exp[Rule]): Exp[Rule] = Optional(r)
+  def &(r: Exp[Rule]): Exp[Rule] = AndPredicate(r)
+  def not(r: Exp[Rule]): Exp[Rule] = NotPredicate(r)
+  def rec(r: => Exp[Rule]): Exp[Rule] = Recursive(() => r)
+
+  def infix_parse(r: Exp[Rule], input: Exp[String]): Exp[Boolean] = Parse(r, input)
+}
+
+trait ScalaGenParboiled extends ScalaGenBase {
+  val IR: ParboiledOpsExp
+  import IR._
+  
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+    case StringLiteral(a) => emitValDef(sym, "StringLiteral" + quote(a))
+    case _ => super.emitNode(sym, rhs)
+  }
+}
+
+class Parsers extends TutorialFunSuite {
+  override val under: String = "parboiled2"
+
+  test("1") {
+    val snippet = new DslDriver[String, Boolean] with ParboiledOpsExp { q =>
+      override val codegen = new DslGen {
+        val IR: q.type = q
+
+        override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+          case StringLiteral(s) => emitValDef(sym, s"true // StringLiteral(${quote(s)})")
+          case Parse(r, input) =>
+            val cursor = fresh[Int]
+            emitVarDecl(cursor)
+            emitValDef(sym, s"true // Parse(${quote(r)}, ${quote(input)}) | cursor: ${quote(cursor)}")
+          case Sequence(l, r) => emitValDef(sym, s"true // Sequence(${quote(l)}, ${quote(r)})")
+          case FirstOf(l, r) => emitValDef(sym, s"true // FirstOf(${quote(l)}, ${quote(r)})")
+          case ZeroOrMore(r) => emitValDef(sym, s"true // ZeroOrMore(${quote(r)})")
+          case OneOrMore(r) => emitValDef(sym, s"true // OneOrMore(${quote(r)})")
+          case Optional(r) => emitValDef(sym, s"true // Optional(${quote(r)})")
+          case AndPredicate(r) => emitValDef(sym, s"true // AndPredicate(${quote(r)})")
+          case NotPredicate(r) => emitValDef(sym, s"true // NotPredicate(${quote(r)})")
+          case Recursive(r) => emitValDef(sym, s"true // Recursive($r)")
+          case _ => super.emitNode(sym, rhs)
+        }
+      }
+
+      def InputLine = &(A ~ "c") ~ oneOrMore("a") ~ B ~ not("a" | "b" | "c")
+      def A: Rep[Rule] = "a" ~ optional(rec(A)) ~ "b"
+      def B: Rep[Rule] = "b" ~ optional(rec(B)) ~ "c"
+
+      def snippet(x: Rep[String]) = {
+        InputLine.parse(x)
+      }
+    }
+    snippet.dumpGeneratedCode = true
+    println(snippet.eval("abc"))
+  }
+}
