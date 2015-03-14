7ebc3919abb240654c19e978d1ec27f7ad6add8b
Alexander Myltsev <alexander.myltsev@phystech.edu>
2015-03-14 12:17:23 +0400

    Extract codegen to a class

--- src/test/scala/org/parboiled2/lms/Parsers.scala
+++ src/test/scala/org/parboiled2/lms/Parsers.scala
@@ -1,6 +1,6 @@
 package org.parboiled2.lms
 
-import scala.lms.tutorial.{DslDriver, DslGen, TutorialFunSuite}
+import scala.lms.tutorial._
 import scala.virtualization.lms.common.{Base, BaseExp, ScalaGenBase}
 
 trait ParboiledOps extends Base {
@@ -54,39 +54,39 @@ trait ParboiledOpsExp extends ParboiledOps with BaseExp {
   def parseInput(r: Exp[Rule], input: Exp[String]): Exp[Boolean] = Parse(r, input)
 }
 
+abstract class ParboiledScalaCodeGen[A:Manifest,B:Manifest] extends DslDriver[A,B] with ParboiledOpsExp { q =>
+  override val codegen = new DslGen {
+    val IR: q.type = q
+
+    override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+      case StringLiteral(s) => emitValDef(sym, s"true // StringLiteral(${quote(s)})")
+      case Parse(r, input) =>
+        val cursor = fresh[Int]
+        emitVarDecl(cursor)
+        emitValDef(sym, s"true // Parse(${quote(r)}, ${quote(input)}) | cursor: ${quote(cursor)}")
+      case Sequence(l, r) => emitValDef(sym, s"true // Sequence(${quote(l)}, ${quote(r)})")
+      case FirstOf(l, r) => emitValDef(sym, s"true // FirstOf(${quote(l)}, ${quote(r)})")
+      case ZeroOrMore(r) => emitValDef(sym, s"true // ZeroOrMore(${quote(r)})")
+      case OneOrMore(r) => emitValDef(sym, s"true // OneOrMore(${quote(r)})")
+      case Optional(r) => emitValDef(sym, s"true // Optional(${quote(r)})")
+      case AndPredicate(r) => emitValDef(sym, s"true // AndPredicate(${quote(r)})")
+      case NotPredicate(r) => emitValDef(sym, s"true // NotPredicate(${quote(r)})")
+      case Recursive(rn, r) => emitValDef(sym, s"true // Recursive($rn, $r)")
+      case _ => super.emitNode(sym, rhs)
+    }
+  }
+}
+
 class Parsers extends TutorialFunSuite {
   override val under: String = "parboiled2"
 
   test("1") {
-    val snippet = new DslDriver[String, Boolean] with ParboiledOpsExp { q =>
-      override val codegen = new DslGen {
-        val IR: q.type = q
-
-        override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
-          case StringLiteral(s) => emitValDef(sym, s"true // StringLiteral(${quote(s)})")
-          case Parse(r, input) =>
-            val cursor = fresh[Int]
-            emitVarDecl(cursor)
-            emitValDef(sym, s"true // Parse(${quote(r)}, ${quote(input)}) | cursor: ${quote(cursor)}")
-          case Sequence(l, r) => emitValDef(sym, s"true // Sequence(${quote(l)}, ${quote(r)})")
-          case FirstOf(l, r) => emitValDef(sym, s"true // FirstOf(${quote(l)}, ${quote(r)})")
-          case ZeroOrMore(r) => emitValDef(sym, s"true // ZeroOrMore(${quote(r)})")
-          case OneOrMore(r) => emitValDef(sym, s"true // OneOrMore(${quote(r)})")
-          case Optional(r) => emitValDef(sym, s"true // Optional(${quote(r)})")
-          case AndPredicate(r) => emitValDef(sym, s"true // AndPredicate(${quote(r)})")
-          case NotPredicate(r) => emitValDef(sym, s"true // NotPredicate(${quote(r)})")
-          case Recursive(rn, r) => emitValDef(sym, s"true // Recursive($rn, $r)")
-          case _ => super.emitNode(sym, rhs)
-        }
-      }
-
+    val snippet = new ParboiledScalaCodeGen[String, Boolean] {
       def InputLine = &(A ~ "c") ~ oneOrMore("a") ~ B ~ !(str("a") | "b" | "c")
       def A: Rep[Rule] = str("a") ~ optional(rec("A", A)) ~ "b"
       def B: Rep[Rule] = str("b") ~ optional(rec("B", B)) ~ "c"
 
-      def snippet(x: Rep[String]) = {
-        InputLine.parse(x)
-      }
+      def snippet(x: Rep[String]) = InputLine.parse(x)
     }
     snippet.dumpGeneratedCode = true
     println(snippet.eval("abc"))
